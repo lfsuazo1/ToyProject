de562f3a337b46b8d2dd95c947e470df6ef17fcd
lfsuazo1 <luis.suazo@helloiconic.com>
2023-03-02 08:59:49 -0600

    Added Admnistrate

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem "stimulus-rails"
 gem "jbuilder"
 
 gem 'devise'
+
+gem "administrate"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :questions
+      resources :users
+
+      root to: "questions#index"
+    end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
